4b3390847b536145438fad8d35941462e4ba1f4f
kinnalrutu-ops <kinnalrutu@gmail.com>
2025-10-20 11:33:53 +0530

    save

---
+++ student_grade_evalution.py
@@ -0,0 +1,15 @@
+print("student_grade_evaluation")
+print("enter your marks:")
+marks = int(input())
+if marks >= 85:
+    print("A")
+elif marks >= 75:
+    print("B")
+elif marks >= 65:
+    print("C")
+elif marks >= 55:
+    print("D")
+else:
+    print("FAIL")
+    
+
